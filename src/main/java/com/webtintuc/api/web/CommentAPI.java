96f2182406d2193f7fe32c9ad66586de083c6384
vantoan1511 <vantoan1517@gmail.com>
2023-09-23 14:55:06 +0700

    CommentAPI: optimized

--- src/main/java/com/webtintuc/api/web/CommentAPI.java
+++ src/main/java/com/webtintuc/api/web/CommentAPI.java
@@ -1,9 +1,9 @@
 package com.webtintuc.api.web;
 
-import com.fasterxml.jackson.databind.ObjectMapper;
 import com.webtintuc.model.Comment;
 import com.webtintuc.model.Model;
 import com.webtintuc.service.ICommentService;
+import com.webtintuc.util.ApiUtils;
 
 import javax.inject.Inject;
 import javax.servlet.ServletException;
@@ -12,7 +12,6 @@ import javax.servlet.http.HttpServlet;
 import javax.servlet.http.HttpServletRequest;
 import javax.servlet.http.HttpServletResponse;
 import java.io.IOException;
-import java.util.stream.Collectors;
 
 @WebServlet(urlPatterns = "/v1/api/comments/")
 public class CommentAPI extends HttpServlet {
@@ -26,34 +25,19 @@ public class CommentAPI extends HttpServlet {
 
     @Override
     protected void doPost(HttpServletRequest req, HttpServletResponse resp) throws ServletException, IOException {
-        req.setCharacterEncoding("UTF-8");
-        resp.setContentType("application/json");
-        ObjectMapper mapper = new ObjectMapper();
-        String payload = req.getReader().lines().collect(Collectors.joining());
-        Comment comment = new Comment();
-        comment = mapper.readValue(payload, Comment.class);
-        comment = commentService.create(comment);
-        mapper.writeValue(resp.getOutputStream(), comment);
+        ApiUtils.init(req, resp);
+        ApiUtils.returnJsonData(resp, commentService.create(ApiUtils.parseRequestBody(req, Comment.class)));
     }
 
     @Override
     protected void doPut(HttpServletRequest req, HttpServletResponse resp) throws ServletException, IOException {
-        req.setCharacterEncoding("UTF-8");
-        resp.setContentType("application/json");
-        ObjectMapper mapper = new ObjectMapper();
-        String payload = req.getReader().lines().collect(Collectors.joining());
-        Comment comment = new Comment();
-        comment = mapper.readValue(payload, Comment.class);
-        comment = commentService.update(comment);
-        mapper.writeValue(resp.getOutputStream(), comment);
+        ApiUtils.init(req, resp);
+        ApiUtils.returnJsonData(resp, commentService.update(ApiUtils.parseRequestBody(req, Comment.class)));
     }
 
     @Override
     protected void doDelete(HttpServletRequest req, HttpServletResponse resp) throws ServletException, IOException {
-        ObjectMapper mapper = new ObjectMapper();
-        String payload = req.getReader().lines().collect(Collectors.joining());
-        Model model = mapper.readValue(payload, Model.class);
-        commentService.delete(model.getIds());
-        mapper.writeValue(resp.getOutputStream(), "");
+        commentService.delete(ApiUtils.parseRequestBody(req, Model.class).getIds());
+        ApiUtils.returnJsonData(resp, "");
     }
 }
---
+++ src/main/java/com/webtintuc/util/ApiUtils.java
@@ -0,0 +1,45 @@
+package com.webtintuc.util;
+
+import com.fasterxml.jackson.databind.ObjectMapper;
+
+import javax.servlet.http.HttpServletRequest;
+import javax.servlet.http.HttpServletResponse;
+import java.io.IOException;
+import java.io.UnsupportedEncodingException;
+import java.util.stream.Collectors;
+
+public class ApiUtils {
+
+    public static void init(HttpServletRequest req, HttpServletResponse resp) throws UnsupportedEncodingException {
+        setCharacterEncoding(req);
+        setJsonContentType(resp);
+    }
+
+    public static <T> T parseRequestBody(HttpServletRequest req, Class<T> tclass) {
+        try {
+            ObjectMapper mapper = new ObjectMapper();
+            String payload = req.getReader().lines().collect(Collectors.joining());
+            return mapper.readValue(payload, tclass);
+        } catch (IOException e) {
+            throw new RuntimeException(e);
+        }
+
+    }
+
+    public static void returnJsonData(HttpServletResponse resp, Object object) {
+        try {
+            ObjectMapper mapper = new ObjectMapper();
+            mapper.writeValue(resp.getOutputStream(), object);
+        } catch (IOException e) {
+            throw new RuntimeException(e);
+        }
+    }
+
+    public static void setJsonContentType(HttpServletResponse resp) {
+        resp.setContentType("application/json");
+    }
+
+    public static void setCharacterEncoding(HttpServletRequest req) throws UnsupportedEncodingException {
+        req.setCharacterEncoding("UTF-8");
+    }
+}
