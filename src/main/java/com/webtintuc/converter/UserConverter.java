fc91fa563c4d287f5d66c79a6e5b82808ef48b36
vantoan1511 <vantoan1517@gmail.com>
2023-09-28 14:45:40 +0700

    Add UserConverter

---
+++ src/main/java/com/webtintuc/converter/UserConverter.java
@@ -0,0 +1,23 @@
+package com.webtintuc.converter;
+
+import com.webtintuc.model.User;
+
+import java.lang.reflect.Field;
+
+public class UserConverter {
+    public static void convert(User des, User src) {
+        Class<?> userClass = User.class;
+        Field[] fields = userClass.getDeclaredFields();
+        for (Field field : fields) {
+            try {
+                field.setAccessible(true);
+                Object value = field.get(src);
+                if (value != null) {
+                    field.set(des, value);
+                }
+            } catch (IllegalAccessException e) {
+                throw new RuntimeException(e);
+            }
+        }
+    }
+}
