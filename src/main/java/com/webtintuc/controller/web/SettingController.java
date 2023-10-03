3d9755a13f68887d556f43dc1baf4fcd33dc0363
vantoan1511 <vantoan1517@gmail.com>
2023-10-03 16:30:08 +0700

    add SettingController

---
+++ src/main/java/com/webtintuc/controller/web/SettingController.java
@@ -0,0 +1,16 @@
+package com.webtintuc.controller.web;
+
+import javax.servlet.ServletException;
+import javax.servlet.annotation.WebServlet;
+import javax.servlet.http.HttpServlet;
+import javax.servlet.http.HttpServletRequest;
+import javax.servlet.http.HttpServletResponse;
+import java.io.IOException;
+
+@WebServlet(urlPatterns = "/settings")
+public class SettingController extends HttpServlet {
+    @Override
+    protected void doGet(HttpServletRequest req, HttpServletResponse resp) throws ServletException, IOException {
+
+    }
+}
